62c73a0ab2ec4b6d7a74bb8b5cdce38047292a5a
LimJoongSub <quhb2455@naver.com>
2020-06-22 00:43:56 +0900

    java calculator
    함수화 및 로그, 지수, 루트 계산 넣음

--- java_study/javastudy/main.java
+++ java_study/javastudy/main.java
@@ -1,12 +1,12 @@
 package javastudy;
 
-import java.io.Console;
 import java.util.*;
 
 
 public class main {
-
+	
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 
 		//expression 배열에 식을 넣기 전처리를 위한 배열
@@ -17,11 +17,10 @@ public class main {
 		int arryleng = 0;
 		//입력식 계산 완료 후 더 계산 할려는지 묻기 위한 변수
 		String chkMore;
+		
 		//scanner로 입력는 객체 생성.
 		Scanner scanner = new Scanner(System.in);
 		
-		//계산 함수가 있는 클래스 객체 생성.
-		Calculator cal = new Calculator();
 		
 		while(true)
 		{
@@ -51,102 +50,25 @@ public class main {
 	 			expression[j] = before_chk[j];
 	 			System.out.print(expression[j]);
 	 		}
+
+			//계산 함수가 있는 클래스 객체 생성 및 생성자로 arryleng를 넘김
+			Calculator cal = new Calculator(arryleng);
+			
+	 		//calculator클래스에서 계산 시 인덱스 조절을 위해 
+	 		cal.arryleng = arryleng;
 	 		
+	 		//제곱, log, 루트 계산
+	 		expression = cal.calculatorForLog(expression, cal.arryleng);
+
 	 		
 	 		//곱셈과 나누셈 먼저 처리
-	 		for(int i = 1; i < arryleng; i ++)
-	 		{
-	 			if( expression[i].equals("*"))
-				{
-	 				//test[i] 앞뒤에 있는 값을 double형으로 변환 후 곱해서 다시 String형으로 변환해서 test[i-1]에 넣음.
-	 				expression[i-1] = Double.toString(cal.product(Double.parseDouble(expression[i-1]), Double.parseDouble(expression[i+1])));
-	 				
-	 				//System.out.println((i-1) + " 번째 값 : " +expression[i-1]);
-	 				//System.out.println("배열 크기 : " + expression.length);
-	 				expression = cal.moveIndex(expression, i-1);
-					arryleng -= 2;
-			 		/*
-					for(int j = 0; j < expression.length;j ++)
-			 		{
-			 			System.out.println(expression[j]);
-			 		}
-			 		*/
-			 		//System.out.println("i값 : " + i);
-			 		//System.out.println("arryleng 값  : " + arryleng);
-			 		i--;
-	 				//continue;
-				}
-	 			
-	 			if( expression[i].equals("/"))
-				{
-	 				//test[i] 앞뒤에 있는 값을 double형으로 변환 후 곱해서 다시 String형으로 변환해서 test[i-1]에 넣음.
-	 				expression[i-1] = Double.toString(cal.divide(Double.parseDouble(expression[i-1]), Double.parseDouble(expression[i+1])));
-
-	 				//System.out.println((i-1) + " 번째 값 : " +expression[i-1]);
-	 				
-	 				expression = cal.moveIndex(expression, i-1);
-					arryleng -= 2;
-					
-					/*
-					for(int j = 0; j < expression.length;j ++)
-			 		{
-			 			System.out.println(expression[j]);
-			 		}
-			 		*/
-					//System.out.println("i값 : " + i);
-			 		//System.out.println("arryleng 값  : " + arryleng);
-			 		i--;
-					//continue;
-				}
-	 			
-	 		}
+	 		expression = cal.calculatorForProdcut_Divide(expression, cal.arryleng);
 	 		
-	 		
+	 		 		
 	 		//덧셈 뺄셈 처리
-	 		for(int i = 0; i < arryleng; i ++)
-	 		{
-	 			if(expression[i].equals("+"))
-	 			{
-	 				expression[i-1] = Double.toString(cal.plus(Double.parseDouble(expression[i-1]), Double.parseDouble(expression[i+1])));
-
-	 				//System.out.println((i-1) + " 번째 값 : " +expression[i-1]);
-	 					
-	 				expression = cal.moveIndex(expression, i-1);
-	 				arryleng -= 2;
-	 				
-	 				/*
-	 				for(int j = 0; j < expression.length;j ++)
-	 		 		{
-	 		 			System.out.println(expression[j]);
-	 		 		}
-	 				System.out.println("i값 : " + i);
-	 		 		System.out.println("arryleng 값  : " + arryleng);
-	 		 		*/
-	 		 		i--;
-
-	 			}
-	 			
-	 			if(expression[i].equals("-"))
-	 			{
-	 				expression[i-1] = Double.toString(cal.minus(Double.parseDouble(expression[i-1]), Double.parseDouble(expression[i+1])));
-
-	 				//System.out.println((i-1) + " 번째 값 : " +expression[i-1]);
-	 					
-	 				expression = cal.moveIndex(expression, i-1);
-	 				arryleng -= 2;
-	 				/*
-	 				for(int j = 0; j < expression.length;j ++)
-	 		 		{
-	 		 			System.out.println(expression[j]);
-	 		 		}
-	 				System.out.println("i값 : " + i);
-	 		 		System.out.println("arryleng 값  : " + arryleng);
-	 		 		*/
-	 		 		i--;
-
-	 			}
-	 			
-	 		}
+	 		expression = cal.calculatorForPlus_Minus(expression, cal.arryleng);
+	 		
+	 		
 	 		
 	 		//System.out.println("입력 식 정답 : " + expression[0]);
 	 		System.out.println(expression[0]);
@@ -163,6 +85,8 @@ public class main {
 
 
 		}
+		
+		
 	}
 
 }
